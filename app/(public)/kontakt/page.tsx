c07ae01cd449658c1f2e788fa8b57fc576698467
Krystian <krystian@bpcoders.pl>
2026-06-04 12:27:05 +0200

    feat: create contact page and add navigation link to header menu

---
+++ app/(public)/kontakt/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next"
+import ContactClientPage from "./ContactClientPage"
+
+export const metadata: Metadata = {
+  title: "Kontakt | Prosta Sprawa",
+  description: "Skontaktuj się z ProstaSprawa.pl. Masz pytania? Skorzystaj z formularza kontaktowego lub znajdź nasze dane kontaktowe i dane rejestrowe.",
+}
+
+export default function KontaktPage() {
+  return <ContactClientPage />
+}
