fe809cba6776b4ea7725af6e79ee118a0910cf73
Krystian <krystian@bpcoders.pl>
2026-06-12 03:08:26 +0200

    feat: add VisuallyHidden component and implement accessible title in command dialog

---
+++ components/ui/visually-hidden.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import * as VisuallyHiddenPrimitive from "@radix-ui/react-visually-hidden"
+
+function VisuallyHidden({
+  ...props
+}: React.ComponentProps<typeof VisuallyHiddenPrimitive.Root>) {
+  return <VisuallyHiddenPrimitive.Root data-slot="visually-hidden" {...props} />
+}
+
+export { VisuallyHidden }
